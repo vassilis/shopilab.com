4c1dfd3bf9a2e0714a2c99bf88701a2f76031b65
Vassilis <vas.terzo@gmail.com>
2019-01-26 13:34:40 +0200

    update services

--- pages/services.js
+++ pages/services.js
@@ -19,6 +19,37 @@ function About(props) {
   return (
     <div className="container-md" style={{ marginTop: 50 }}>
       <Navbar />
+      <Typography className="hero" style={{ fontSize: "5rem", lineHeight: 1 }}>
+        Custom software solutions tailored to your unique needs
+      </Typography>
+      <Typography style={{ fontSize: "1.2rem", marginTop: 40, maxWidth: 1000 }}>
+        Expertise... experience / since 2010, rapid prototyping, MVPs, SAAS,
+        proof of concepts, customer engagement, sales analytics
+      </Typography>
+      <Typography variant="h4" style={{ marginTop: 40 }}>
+        Clients / Industries
+      </Typography>
+      <Typography style={{ fontSize: "1.2rem", marginTop: 5, maxWidth: 1000 }}>
+        Siemens, Wesco, Rittal, ...
+      </Typography>
+      <Typography variant="h4" style={{ marginTop: 40 }}>
+        Attitude
+      </Typography>
+      <Typography style={{ fontSize: "1.2rem", marginTop: 5, maxWidth: 1000 }}>
+        Friendly, ethical, constant communication...
+      </Typography>
+      <Typography variant="h4" style={{ marginTop: 40 }}>
+        Methodology
+      </Typography>
+      <Typography style={{ fontSize: "1.2rem", marginTop: 5, maxWidth: 1000 }}>
+        Agile, Scrum, ...
+      </Typography>
+      <Typography variant="h4" style={{ marginTop: 40 }}>
+        Our Stack
+      </Typography>
+      <Typography style={{ fontSize: "1.2rem", marginTop: 5, maxWidth: 1000 }}>
+        Friendly, constant communication...
+      </Typography>
     </div>
   );
 }
