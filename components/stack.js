be42cbbbd4cd9dd4c0c2b987352efc420ea823f6
Vassilis <vas.terzo@gmail.com>
2019-01-26 10:19:19 +0200

    update stack component

--- components/stack.js
+++ components/stack.js
@@ -17,22 +17,26 @@ const styles = () => ({
     flexWrap: "wrap",
     padding: 50
   },
-  img: {
+  img130: {
     margin: 20,
     maxWidth: 130
+  },
+  img180: {
+    margin: 20,
+    maxWidth: 180
   }
 });
 
 const Stack = ({ classes }) => (
   <div className={classes.root}>
-    <img className={classes.img} src={LogoAws} alt="AWS" />
-    <img className={classes.img} src={LogoHeroku} alt="Heroku" />
-    <img className={classes.img} src={LogoNode} alt="Node" />
-    <img className={classes.img} src={LogoReact} alt="React" />
-    <img className={classes.img} src={LogoRedux} alt="Redux" />
-    <img className={classes.img} src={LogoRails} alt="Ruby on Rails" />
-    <img className={classes.img} src={LogoPostgres} alt="PostgreSQL" />
-    <img className={classes.img} src={LogoHighcharts} alt="Highcharts" />
+    <img className={classes.img130} src={LogoAws} alt="AWS" />
+    <img className={classes.img130} src={LogoHeroku} alt="Heroku" />
+    <img className={classes.img130} src={LogoNode} alt="Node" />
+    <img className={classes.img130} src={LogoReact} alt="React" />
+    <img className={classes.img130} src={LogoRedux} alt="Redux" />
+    <img className={classes.img130} src={LogoRails} alt="Ruby on Rails" />
+    <img className={classes.img180} src={LogoPostgres} alt="PostgreSQL" />
+    <img className={classes.img180} src={LogoHighcharts} alt="Highcharts" />
   </div>
 );
 
