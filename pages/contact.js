e86327ea59b13dcdb03b13fcd0f579a8b3fc17c8
Vassilis <vas.terzo@gmail.com>
2019-01-26 14:50:49 +0200

    add contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,30 +1,118 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
-import Link from "next/link";
 import Navbar from "../components/navbar";
+import TextField from "@material-ui/core/TextField";
+import { Grid, Button } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
-const styles = theme => ({
-  root: {
-    textAlign: "center",
-    paddingTop: theme.spacing.unit * 20
+class ContactPage extends React.Component {
+  state = { firstName: "", lastName: "", message: "", email: "", phone: "" };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  render() {
+    const { firstName, lastName, email, phone, message } = this.state;
+    return (
+      <div className="container-md" style={{ marginTop: 50 }}>
+        <Navbar />
+        <div className="container" style={{ padding: "50px 24px" }}>
+          <Typography variant="h2" className="hero" gutterBottom>
+            <strong>
+              We love to discuss and analyse business stories! Let's get some
+              real work done!
+            </strong>
+          </Typography>
+          <br />
+          <form
+            name="contact"
+            method="post"
+            data-netlify-honeypot="bot-field"
+            data-netlify="true"
+          >
+            <input type="hidden" name="bot-field" />
+            <Grid container spacing={24}>
+              <Grid item sm={6}>
+                <TextField
+                  required
+                  id="firstName"
+                  name="firstName"
+                  label="First Name"
+                  value={firstName}
+                  onChange={this.handleChange("firstName")}
+                  margin="normal"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item sm={6}>
+                <TextField
+                  required
+                  id="lastName"
+                  name="lastName"
+                  label="Last Name"
+                  value={lastName}
+                  onChange={this.handleChange("lastName")}
+                  margin="normal"
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={24}>
+              <Grid item sm={6}>
+                <TextField
+                  required
+                  id="email"
+                  name="email"
+                  type="email"
+                  label="Email Address"
+                  value={email}
+                  onChange={this.handleChange("email")}
+                  margin="normal"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item sm={6}>
+                <TextField
+                  required
+                  id="phone"
+                  name="phone"
+                  type="tel"
+                  label="Phone Number"
+                  value={phone}
+                  onChange={this.handleChange("phone")}
+                  margin="normal"
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <br />
+            <TextField
+              required
+              id="message"
+              name="message"
+              label="What can we do for you?"
+              multiline
+              fullWidth
+              rowsMax="10"
+              value={message}
+              onChange={this.handleChange("message")}
+              margin="normal"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{ marginTop: 20 }}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
-});
-
-function About(props) {
-  const { classes } = props;
-
-  return (
-    <div className="container-md" style={{ marginTop: 50 }}>
-      <Navbar />
-    </div>
-  );
 }
 
-About.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(About);
+export default ContactPage;
